5d2fbda996a255e46758ad4795533496d5692f80
enricogiacomin <e.giacomin@jacknet.biz>
2023-02-13 09:38:08 +0100

    minor changes

--- Migrate-RecConnectors10to16.ps1
+++ Migrate-RecConnectors10to16.ps1
@@ -7,14 +7,16 @@ $SourceServer = "Mailserv"
 $DestServer = "srvsvc03"
 
 
-$RC = Get-ReceiveConnector -Server $SourceServer | Select-Object -Property Identity,Name,Enabled,ProtocolLoggingLevel,MaxMessageSize,MaxLocalHopCount,MaxHopCount,AuthMechanism,PermissionGroups,RemoteIPRanges,Bindings,FQDN
+$RC = Get-ReceiveConnector -Server $SourceServer | Select-Object -Property Identity, Name, Enabled, ProtocolLoggingLevel, MaxMessageSize, MaxLocalHopCount, MaxHopCount, AuthMechanism, PermissionGroups, RemoteIPRanges, Bindings, FQDN
 
-foreach ($r in $RC){
+foreach ($r in $RC) {
     $dom = Read-Host "Vuoi creare sul nuovo server il connettore di ricezione"$r.name"?(S/N)"
-    if ($dom -eq "S"){
-    Write-Host -ForegroundColor Green "Creazione del connettore $r.name"
-    New-ReceiveConnector -Server $DestServer -Name $r.Name -TransportRole FrontendTransport -AuthMechanism $r.AuthMechanism -PermissionGroups $r.PermissionGroups -RemoteIPRanges $r.RemoteIPRanges -Fqdn $r.Fqdn -ProtocolLoggingLevel $r.ProtocolLoggingLevel -Bindings 0.0.0.0:25 #-WhatIf
+    if ($dom -eq "S") {
+        Write-Host -ForegroundColor Green "Creazione del connettore $r.name"
+        New-ReceiveConnector -Server $DestServer -Name $r.Name -TransportRole FrontendTransport -AuthMechanism $r.AuthMechanism -PermissionGroups $r.PermissionGroups -RemoteIPRanges $r.RemoteIPRanges -Fqdn $r.Fqdn -ProtocolLoggingLevel $r.ProtocolLoggingLevel -Bindings 0.0.0.0:25 #-WhatIf
     }
-    else{
-    Write-Host -ForegroundColor red "Skip"
+    else {
+        Write-Host -ForegroundColor red "Skip"
+    }
+}
   
